5e04c1a3b2a740e164f90eed53649268d88c593e
devanshtangri <deva.tangri@gmail.com>
2025-12-18 22:48:01 +0530

    Worked on operator logic

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ const buttons = document.querySelectorAll("button");
 
 
 //Helper Variables //
-let first = "", second = "", result;
+let displayNumber = "", first, result;
 //Helper Variables //
 
 
 function numberInput (input) {
-    if (/^\d$/.test(input.key) || (input.key === "." && !first.includes("."))) {
-        first += input.key;
-        display.innerHTML = first;
+    if (/^\d$/.test(input.key) || (input.key === "." && !displayNumber.includes("."))) {
+        displayNumber += input.key;
+        display.innerHTML = displayNumber;
     }
 }
 function enableFunctions (input) {
-    if (/^\d$/.test(input.key) || (input.key === "." && !first.includes("."))) {
+    if (/^\d$/.test(input.key) || (input.key === "." && !displayNumber.includes("."))) {
         buttons.forEach((v) => { v.disabled = false; });
         document.removeEventListener("keypress", enableFunctions);
     }
@@ -31,21 +31,55 @@ function applyModifier (input) {
     if (input.target.innerHTML === "AC") {
         subExp.innerHTML = "";
         display.innerHTML = "0";
-        first = "";
+        displayNumber = "";
         modifiers.forEach((v) => { v.disabled = true; });
         operators.forEach((v) => { v.disabled = true; });
         document.addEventListener("keypress", enableFunctions);
     } else if (input.target.innerHTML === "+/-") {
-        first = String(Number(first) * -1);
-        display.innerHTML = first;
+        displayNumber = String(Number(displayNumber) * -1);
+        display.innerHTML = displayNumber;
     } else if (input.target.innerHTML === "%") {
-        first = String(Number(first) / 100);
-        display.innerHTML = first;
+        displayNumber = String(Number(displayNumber) / 100);
+        display.innerHTML = displayNumber;
     }
 }
 
+function applyOperatorKeypress (input) {
+    if ("+-*/".includes(input.key)) {
+        if (subExp.innerHTML === "") {
+            subExp.innerHTML = `${displayNumber} ${input.key} `;
+            displayNumber = "";
+            display.innerHTML = "0";
+        } else {
+            subExp.innerHTML.charAt(subExp.innerHTML.length - 2) = input.key;
+        }
+    }
+}
+function applyOperatorClick (input) {
+    if ("+-*/".includes(input.target.innerHTML)) {
+        if (subExp.innerHTML === "") {
+            subExp.innerHTML = `${displayNumber} ${input.target.innerHTML} `;
+            displayNumber = "";
+            display.innerHTML = "0";
+        } else {
+            subExp.innerHTML.charAt(subExp.innerHTML.length - 2) = input.target.innerHTML;
+        }
+    }
+}
 
+function calculateResult () {
+
+}
 document.addEventListener("keypress", numberInput);
 document.addEventListener("keypress", enableFunctions);
 
-modifiers.forEach((v) => { v.addEventListener("click", applyModifier) });
+modifiers.forEach((v) => {
+    v.addEventListener("click", applyModifier);
+});
+
+operators.forEach((v) => {
+    v.addEventListener("click", applyOperatorClick);
+    v.addEventListener("keypress", applyOperatorKeypress);
+});
+
+calculate.addEventListener("click", calculateResult);
